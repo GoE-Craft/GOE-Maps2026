04835650aed7a19299f9fc418671eb1aacf0d8a2
slovke024 <76702780+slovke024@users.noreply.github.com>
2026-02-05 10:48:23 +0100

    TNT Addon - Lightning tnt update

--- GoE/TNTplusAddOn_1.0.0/Bedrock_Template/Content/world_template/behavior_packs/TNT_BP/scripts/tnt/actions/lightning_tnt.js
+++ GoE/TNTplusAddOn_1.0.0/Bedrock_Template/Content/world_template/behavior_packs/TNT_BP/scripts/tnt/actions/lightning_tnt.js
@@ -1,4 +1,12 @@
-const LIGHTNING_RADIUS = 5;
+import { system } from "@minecraft/server";
+
+const LIGHTNING_RADIUS = 7;
+const EXTRA_STRIKES = 4;
+const TICKS_BETWEEN_EXTRA = 4;
+
+function randomOffset(radius) {
+    return (Math.random() * 2 - 1) * radius;
+}
 
 export function lightningAction(dimension, location, entity) {
     try {
@@ -18,6 +26,22 @@ export function lightningAction(dimension, location, entity) {
                 });
             } catch (err) {
             }
+        }
+
+        for (let i = 0; i < EXTRA_STRIKES; i++) {
+            const delayTicks = i * TICKS_BETWEEN_EXTRA;
+            system.runTimeout(() => {
+                try {
+                    const x = location.x + randomOffset(LIGHTNING_RADIUS);
+                    const z = location.z + randomOffset(LIGHTNING_RADIUS);
+                    dimension.spawnEntity("minecraft:lightning_bolt", {
+                        x,
+                        y: location.y,
+                        z
+                    });
+                } catch (err) {
+                }
+            }, delayTicks);
         }
     } catch (e) {
     }
